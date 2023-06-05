f67e8d25aed750ec6556eac513fabebdd71e73c2
vtrcavassana <victor.cavassana@outlook.com>
2023-06-05 01:23:49 -0300

    docs: Added comments to routes.py

--- Projeto1/routes.py
+++ Projeto1/routes.py
@@ -1,7 +1,10 @@
+# Importando as bibliotecas necessárias
 from fastapi import APIRouter
 
+# Iniciando o roteador
 roteador = APIRouter()
 
+# Rota que retorna uma string quando '/conversor' é acessada
 @roteador.get('/conversor')
 def conversor():
     return 'Funcionou!'
