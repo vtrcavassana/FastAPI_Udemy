4255ed163bbf03bc9ac274dbc423d1826de091ba
vtrcavassana <victor.cavassana@outlook.com>
2023-06-05 01:23:04 -0300

    docs: Added comments to main.py

--- Projeto1/main.py
+++ Projeto1/main.py
@@ -1,9 +1,14 @@
+# Importando as bibliotecas necessárias
 from fastapi import FastAPI
 from routes import roteador
 
+# Iniciando a aplicação FastAPI
 app = FastAPI()
+
+# Incluindo o roteador na aplicação principal
 app.include_router(router=roteador)
 
+# Rota padrão que retorna uma string quando a URL base é acessada
 @app.get('/')
 def home():
     return 'AQUI FOI!'
