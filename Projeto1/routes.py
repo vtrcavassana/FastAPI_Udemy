f6943367c5c35a9196fc3375ec6c9b7ab7a94c61
vtrcavassana <victor.cavassana@outlook.com>
2023-06-05 21:24:14 -0300

    docs: Add Path Parameters / comments to routes.py

--- Projeto1/routes.py
+++ Projeto1/routes.py
@@ -1,10 +1,17 @@
-# Importando as bibliotecas necessárias
+# Importando a biblioteca fastapi que será utilizada para criar a API
 from fastapi import APIRouter
 
-# Iniciando o roteador
+# Iniciando o roteador da API, que será responsável por direcionar as requisições aos endpoints corretos
 roteador = APIRouter()
 
 # Rota que retorna uma string quando '/conversor' é acessada
-@roteador.get('/conversor')
-def conversor():
+# @roteador.get('/conversor')
+# def conversor():
+#     return 'Funcionou!'
+
+# Path Parameter
+# O parâmetro 'moeda' na rota indica que o valor será fornecido na URL.
+# Por exemplo, se você acessar '/conversor/usd', o valor de 'moeda' será 'usd'.
+@roteador.get('/conversor/{moeda}')
+def conversor(moeda: str):
     return 'Funcionou!'
